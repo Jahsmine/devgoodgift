c9f41da938ff022cb2db78bc7adfd7146b11932f
MathewDyrin <gdiskfordocuments@gmail.com>
2020-06-19 17:24:33 +0300

    Uninstall flask oauthlib

--- src/extensions.py
+++ src/extensions.py
@@ -5,7 +5,6 @@ from flask_jwt_extended import JWTManager
 from flask_httpauth import HTTPBasicAuth
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
-from flask_oauthlib.client import OAuth
 from dotenv import load_dotenv
 
 db = SQLAlchemy()
